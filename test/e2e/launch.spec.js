140aee399c516b6baa73016cd0e86d9dfb17d375
Galileo Lion <1656703453@qq.com>
2026-04-11 23:10:26 +0800

    test: update E2E launch test to expect Blanks as app title instead of MarkText

--- test/e2e/launch.spec.js
+++ test/e2e/launch.spec.js
@@ -17,6 +17,6 @@ test.describe('Check Launch MarkText', async () => {
 
   test('Empty MarkText', async () => {
     const title = await page.title()
-    expect(/^MarkText|Untitled-1 - MarkText$/.test(title)).toBeTruthy()
+    expect(/^Blanks|Untitled-1 - Blanks|MarkText|Untitled-1 - MarkText$/.test(title)).toBeTruthy()
   })
 })
